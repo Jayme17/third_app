9c30eea1ed1ee741800ab0107da2a54dcea2632b
jayme17 <jjones@chicagotechacademy.org>
2014-04-22 13:12:42 -0500

    Done With Stuffffff
    Yayyy

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "UserPages" do
   subject { Page }
+  
   describe "signup page" do
   	before { visit signup_path}
 
@@ -9,4 +10,12 @@ describe "UserPages" do
   	it { should have_selector('title', text: 'Sign Up')}
     
   end
+
+  describe "profile page" do
+  	let(:user) { FactoryGirl.create(:user)}
+  	before { visit signup_path}
+
+  	it { should have_selector('h1', text: user.name) }
+  	it { should have_selector('title', text: user.name)}
+    
 end
